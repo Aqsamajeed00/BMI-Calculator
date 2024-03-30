613e1d695d3c15172a27ac122365f9a8d566eccf
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-03-30 23:23:54 +0500

    "Added new feature to improve user experience"

---
+++ main.js
@@ -0,0 +1,6 @@
+// BMI Calculator
+// Weight in KG.....Height in Meter
+var weight = 70;
+var height = 1.75;
+var bmi = weight / (height * height);
+console.log("Aqsa your BMI is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,13 @@
+
+// BMI Calculator
+//Weight in KG.....Height in Meter
+let weight =70;
+let height = 1.75;
+let bmi = weight/ (height*height)
+console.log(`Aqsa your BMI is ${bmi}`)
+
+
+
+
+
+
